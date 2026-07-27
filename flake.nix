529c2378ef277d77b2c40125278feddb3cba763e
Kaarlo Kirvelä <kkirvela@gmail.com>
2026-07-27 22:11:30 +0300

    added laptop

--- flake.nix
+++ flake.nix
@@ -29,6 +29,28 @@
     }:
     {
       nixosConfigurations = {
+        laptop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/laptop/configuration.nix
+
+            catppuccin.nixosModules.catppuccin
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.kaarlo = {
+                imports = [
+                  ./home/default.nix
+                  catppuccin.homeModules.catppuccin
+                ];
+              };
+              home-manager.backupFileExtension = "backup";
+            }
+          ];
+        };
         desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
 
